21220c395e4fd927ceb51d927234c5300fc63a34
alpEr0640 <alpersonat33@gmail.com>
2024-08-10 12:37:28 +0300

    Login page and navbar complated

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,47 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Homepage from "./Pages/Homepage";
+import Login from "./Pages/Login";
+import { MainProvider, useMainContext } from "./MainContext";
+import { AuthProvider, useAuth } from "./AuthContext";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <MainProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <AppContent />
+        </BrowserRouter>
+      </AuthProvider>
+    </MainProvider>
   );
 }
+
+const AppContent = () => {
+  const [logged, setLogged] = useState(false);
+
+  useEffect(() => {
+    const storedLogged = window.localStorage.getItem("logged");
+    setLogged(storedLogged === "true");
+    console.log("Giriş durumu: ", storedLogged);
+  }, []);
+
+  const { isAuthenticated } = useAuth();
+  const { isLogged } = useMainContext();
+
+  return (
+    <div className="app">
+      {isLogged && <Navbar />}
+      <Routes>
+        <Route
+          path="/homepage"
+          element={isAuthenticated ? <Homepage /> : <Login />}
+        />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
---
+++ client/src/Pages/Homepage.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "../CSS/Homepage.css";
+export default function Homepage() {
+  return <div className="homepageContainer">asdfasd</div>;
+}
